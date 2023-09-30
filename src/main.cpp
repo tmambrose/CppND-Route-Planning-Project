2fc386423ec21c3a5ee8560c3805cd69f9467281
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-09-30 11:24:27 -0700

    Update main.cpp TODO #1
    Add simple solution for TODO #1

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,14 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x {}, start_y {}, end_x {}, end_y {};
 
+    std::cout << "\nEnter starting coordinates X and Y: ";
+    std::cin >> start_x >> start_y;
+
+    std::cout << "\nEnter ending coordinates X and Y: ";
+    std::cin >> end_x >> end_y;
+    
     // Build Model.
     RouteModel model{osm_data};
 
